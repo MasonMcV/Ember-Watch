e479b9c58f3f267bd963291af02957cd22cd1a6a
NolanClark <nolan@DESKTOP-EMLLLEA.localdomain>
2018-10-20 17:00:15 -0500

    more troubleshooting

--- backend/test.py
+++ backend/test.py
@@ -4,4 +4,4 @@ from firebase_admin import credentials
 cred = credentials.Certificate("ember-watch-cb6ea-firebase-adminsdk-nz491-27c62915f8.json")
 firebase_admin.initialize_app(cred)
 
-
+print('done')
