d99992d8cbec1baf2cdb5faa31dc0110b8a507ac
Ben-Floyd <bjfloyd@LAPTOP-AKINLJMT.localdomain>
2018-10-20 17:22:18 -0500

    Troubleshoot create database

--- backend/test.py
+++ backend/test.py
@@ -1,7 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import firestore
 
 cred = credentials.Certificate("ember-watch-cb6ea-firebase-adminsdk-nz491-27c62915f8.json")
 firebase_admin.initialize_app(cred)
+db = firebase_admin.firestore.client()
 
 print('done')
