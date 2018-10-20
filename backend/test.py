8e7a69e7980c5966902bea39fa4e94630f91b16e
Ben-Floyd <bjfloyd@LAPTOP-AKINLJMT.localdomain>
2018-10-20 17:58:51 -0500

    Troubleshoot add to database

--- backend/test.py
+++ backend/test.py
@@ -5,5 +5,19 @@ from firebase_admin import firestore
 cred = credentials.Certificate("ember-watch-cb6ea-firebase-adminsdk-nz491-27c62915f8.json")
 firebase_admin.initialize_app(cred)
 db = firebase_admin.firestore.client()
+emberWatch = db.collection(u'Ember-Watch')
+
+data = {
+    u'Test' : u'Hello World!',
+    u'Proof' : True
+}
+
+emberWatch.document(u'Untitled').set(data)
+
+#gets list of documents in collection
+docs = emberWatch.get()
+
+for doc in docs:
+    print(u'{} => {}'.format(doc.id, doc.to_dict()))
 
 print('done')
