0072796c1527dd14ec466b615b33ae767e0c038c
Naleile <gnwalraven@crimson.ua.edu>
2018-10-21 09:46:54 -0700

    Putting the stuff online

---
+++ ember_watch/lib/drawer/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:ember_watch/drawer/license.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Ember Watch',
+      theme: new ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: new MyHomePage(title: 'Ember Watch'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(title: new Text('Ember Watch')),
+        body: new Text(''),
+        drawer: new Drawer(
+          child: new ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              new DrawerHeader(
+                child: null,
+                decoration: new BoxDecoration( //color: Colors.red),
+                  image: DecorationImage(
+                      image: new AssetImage('assets/EWWN.png')),
+                ),
+              ),
+              new ListTile(
+                  title: new Text('Settings'),
+                  onTap: () {
+                    //update state of app
+                  }),
+              new ListTile(
+                  title: new Text('License'),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(context, new MaterialPageRoute(
+                        builder: (context) => new LicensePg()));
+                  }),
+            ],
+          ),
+        ));
+  }
+
+
+}
