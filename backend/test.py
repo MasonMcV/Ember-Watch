18d6ac49a52cec5601d34fd4d75243e103067599
Ben-Floyd <bjfloyd@LAPTOP-AKINLJMT.localdomain>
2018-10-20 23:06:50 -0500

    Troubleshoot getting from storage

--- backend/test.py
+++ backend/test.py
@@ -1,23 +1,25 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from firebase_admin import storage
 
 cred = credentials.Certificate("ember-watch-cb6ea-firebase-adminsdk-nz491-27c62915f8.json")
-firebase_admin.initialize_app(cred)
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'ember-watch-cb6ea.appspot.com'
+})
 db = firebase_admin.firestore.client()
 emberWatch = db.collection(u'Ember-Watch')
 
-data = {
-    u'Test' : u'Hello World!',
-    u'Proof' : True
-}
+bucket = storage.bucket()
 
-emberWatch.document(u'Untitled').set(data)
+data = {
+   
+}
 
 #gets list of documents in collection
 docs = emberWatch.get()
 
-for doc in docs:
-    print(u'{} => {}'.format(doc.id, doc.to_dict()))
+#for doc in docs:
+   # print(u'{} => {}'.format(doc.id, doc.to_dict()))
 
-print('done')
+print(bucket.exists())
